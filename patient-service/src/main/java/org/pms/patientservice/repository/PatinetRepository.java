84735faa0db9e8e68046737e626d767a90b360a4
Anurag0306 <karamveer0306@gmail.com>
2025-04-08 17:38:14 +0530

    started

--- patient-service/src/main/java/org/pms/patientservice/repository/PatinetRepository.java
+++ patient-service/src/main/java/org/pms/patientservice/repository/PatinetRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//Creating the constructor
+//Creating the repository and extending to JPARepository
 public interface PatinetRepository extends JpaRepository <Patient, UUID> {
 }
